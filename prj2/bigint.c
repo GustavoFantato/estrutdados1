569ebd4d6f77047d79207886ad72da2e93a397e6
GustavoFantato <gugafantato@gmail.com>
2025-11-13 19:02:51 -0300

    Pequenos ajustes

--- prj2/bigint.c
+++ prj2/bigint.c
@@ -3,12 +3,23 @@
 #include <stdlib.h>
 
 
-struct BigInt{
-	int len; // quantidade de dígitos utilizados
-	int cap; // capacidade máxima de dígitos
-	int *head; // ponteiro para o array de dígitos
-	char sinal; // positivo ou negativo
+struct BigInt{ 
+	int size; // quantidade de blocos (cada bloco com 2 dígitos)
+	Node *inicio; // node menos significativo
+	Node *final; // node mais significativo 
+	char sinal; // positivo (POS) ou negativo (NEG)
 };
+
+struct node{
+	int d0; // Digito menos significativo
+	int d1; // Digito mais significativo (do bloco)
+	Node *next; // next node da lista
+	Node *prev; // node anterior da lista
+};
+
+
+
+
 
 
 
